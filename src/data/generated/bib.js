f2c2bddac3830cd5f6484e478a00f39c05d8f04a
Sylviaming <sylvia_wmx568@163.com>
2025-04-24 15:22:54 +0800

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,92 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "asai2023selfrag": {
+        "author": "Asai, Akari and Zhao, Sherry and Liu, Mengzhou and Lee, Kenton and Lewis, Patrick and Zaharia, Matei and Liang, Percy",
+        "booktitle": "ICLR",
+        "doi": "10.48550/arXiv.2310.11511",
+        "keywords": "RAG basic architecture, self-reflection",
+        "title": "Self-RAG: Learning to Retrieve, Generate, and Critique through Self-Reflection",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "chen2023benchmarking": {
+        "author": "Chen, Akari and Xie, Simran M and Liang, Percy",
+        "booktitle": "NeurIPS",
+        "doi": "10.48550/arXiv.2309.01431",
+        "keywords": "evaluation, benchmarking",
+        "title": "Benchmarking Large Language Models in Retrieval-Augmented Generation",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "gao2023rarr": {
+        "author": "Gao, Luyu and Madaan, Aman and Zhou, Shuyan and Alon, Uri and Liu, Pengfei and Yang, Yiming and Callan, Jamie and Neubig, Graham",
+        "booktitle": "ACL",
+        "doi": "10.48550/arXiv.2210.08726",
+        "keywords": "RAG basic architecture, iterative retrieval",
+        "title": "RARR: Researching and Revising What Language Models Say, Using Language Models",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "gao2023retrievalaugmented": {
+        "author": "Gao, Junyi and Xiong, Cong and Si, Chufan and Liu, Qingying and Wang, Sirui and Bennett, Paul N and Dou, Zhicheng",
+        "doi": "10.48550/arXiv.2305.13657",
+        "journal": "ACM Computing Surveys",
+        "keywords": "human-AI interaction, user experience",
+        "title": "Retrieval-Augmented Generation for AI-Generated Content: A Survey",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "year": "2023"
+    },
+    "lewis2020rag": {
+        "author": "Lewis, Patrick and Perez, Ethan and Piktus, Aleksandra and Petroni, Fabio and Karpukhin, Vladimir and Goyal, Naman and K{\\\"u}ttler, Heinrich and Lewis, Mike and Yih, Wen-tau and Rockt{\\\"a}schel, Tim and Riedel, Sebastian and Kiela, Douwe",
+        "booktitle": "NeurIPS",
+        "doi": "10.48550/arXiv.2005.11401",
+        "keywords": "RAG basic architecture",
+        "title": "Retrieval-Augmented Generation for Knowledge-Intensive NLP Tasks",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "ram2023incontext": {
+        "author": "Ram, Ori and Levine, Yoav and Dalmedigos, Itay and Muhlgay, Dor and Sap, Maarten and Leviathan, Yaniv and Kalai, Yoav and Goldberg, Yoav and Shashua, Amnon",
+        "doi": "10.48550/arXiv.2302.00083",
+        "journal": "TACL",
+        "keywords": "retrieval strategy, context integration",
+        "title": "In-Context Retrieval-Augmented Language Models",
+        "type": "article",
+        "year": "2023"
+    },
+    "shi2023ares": {
+        "author": "Shi, Weijia and Dou, Zhicheng and Xu, Jun and Hu, Jiaxin and Xiao, Zewen and Yu, Xiaochen and Liu, Zihan and Wen, Ji-Rong",
+        "booktitle": "SIGIR",
+        "doi": "10.48550/arXiv.2311.09476",
+        "keywords": "evaluation, framework",
+        "title": "ARES: An Automated Evaluation Framework for Retrieval-Augmented Generation Systems",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "vu2023freshllms": {
+        "author": "Vu, Tu and Iyyer, Mohit and Zhu, Jingfei and Neubig, Graham",
+        "booktitle": "EMNLP",
+        "doi": "10.48550/arXiv.2310.03214",
+        "keywords": "domain applications, temporal knowledge",
+        "title": "FreshLLMs: Refreshing Large Language Models with Search Engine Augmentation",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "wang2023querygeneration": {
+        "author": "Wang, Shirong and Li, Xiang and Wu, Sai and Chang, Zhangyang and Zhao, Amy and Zeng, Yingqiang and Wang, Danding and Qian, Haotian and Yang, Fan and Zhang, Huaixiu and Lee, Hongyang R and Tu, Zhaopeng",
+        "booktitle": "ICML",
+        "doi": "10.48550/arXiv.2312.10753",
+        "keywords": "retrieval strategy",
+        "title": "Query Generation for Retrieving Knowledge in RAG: A Comprehensive Survey",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "xiong2023enhancing": {
+        "author": "Xiong, Wei and Sun, Ruosong and Zheng, Xiyou and Lin, Jimmy and Zhang, Min",
+        "booktitle": "AAAI",
+        "doi": "10.48550/arXiv.2310.01558",
+        "keywords": "domain applications, complex QA",
+        "title": "Enhancing Retrieval-Augmented Large Language Models with Iterative Retrieval-Generation Synergy",
+        "type": "inproceedings",
+        "year": "2024"
     }
 };
